17cc561ffe28330504483964b8f116bf3cc59e1f
kubo-ko <kubo.ko@mythos-jp.com>
2014-06-06 16:49:12 +0900

    syuusei

--- src/test_project/Main.java
+++ src/test_project/Main.java
@@ -1,9 +1,13 @@
 package test_project;
 /**
- * マスター 
+ * マスター
  */
 public class Main {
 
+	/**
+	 * main関数
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// branch1
 		System.out.println("");
